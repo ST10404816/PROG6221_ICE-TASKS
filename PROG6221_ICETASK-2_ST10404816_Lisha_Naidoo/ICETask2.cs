47c6db661c24551b77f844e4ff4915565b6b177c
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-04-28 20:13:10 +0200

    ICE Task 2

---
+++ PROG6221_ICETASK-2_ST10404816_Lisha_Naidoo/ICETask2.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace PROG6221_ICETASK_2_ST10404816_Lisha_Naidoo
+{
+	internal class ICETask2
+	{
+		public static void calculate(string n)
+		{
+			double num1=0;
+			double num2=0;
+			double result;
+			bool end=false;
+
+			while (end != true)
+			{
+				int i = 0;
+				if((double.TryParse(n.Substring(i, 1), out num1)==true))
+				{
+					i++;
+					if ((double.TryParse(n.Substring(i, 1), out num2) == true))
+					{
+						end = true;
+					}
+					else
+					{
+						double.TryParse(n.Substring(i+1, 1), out num2);
+						end = true;
+					}
+				}
+				i++;
+			}
+
+			if (n.Contains('+'))
+			{
+				
+				result = num1 + num2;
+				Console.WriteLine(num1 + " + " + num2 + " = " + result);
+
+			}
+			else if (n.Contains('-'))
+			{
+
+				result = num1 - num2;
+				Console.WriteLine(num1 + " - " + num2 + " = " + result);
+
+			}
+			else if (n.Contains('*'))
+			{
+
+				result = num1 * num2;
+				Console.WriteLine(num1 + " x " + num2 + " = " + result);
+
+			}
+			else if (n.Contains('/'))
+			{
+				if(num2 == 0)
+				{
+					Console.WriteLine("Error: Cannot divide by 0");
+				}
+				else
+				{
+					result = num1 / num2;
+					Console.WriteLine(num1 + " ÷ " + num2 + " = " + result);
+				}
+			}
+
+		}
+
+		}
+
+
+
+	}
+}
+		
+
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
---
+++ PROG6221_ICETASK-2_ST10404816_Lisha_Naidoo/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Services;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_ICETASK_2_ST10404816_Lisha_Naidoo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//ICE Task 2
+
+			Console.WriteLine("ICE Task 2\n");
+			Console.WriteLine("---Calculator App---\n");
+
+			string input = " ";
+			while (input != "0")
+			{
+				Console.Write("Enter two numbers and an operator: ");
+				input = Console.ReadLine();
+				ICETask2.calculate(input);
+				Console.WriteLine();
+			}
+
+		}
+
+	}
+}
+
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
