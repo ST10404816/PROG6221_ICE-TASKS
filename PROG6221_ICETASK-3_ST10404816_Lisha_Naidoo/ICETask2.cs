8475446277c2b0b0a1d62f5027ec0df103c587e5
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-04-28 20:42:33 +0200

    ICE Task 3

---
+++ PROG6221_ICETASK-3_ST10404816_Lisha_Naidoo/ICETask2.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_ICETASK_3_ST10404816_Lisha_Naidoo
+{
+	internal class ICETask2
+	{
+		//Variables
+		private string customerId;
+		private int monthlyUsage1;
+		private int monthlyUsage2;
+		private int monthlyUsage3;
+
+		//Constructor
+		public ICETask2(string customerId, int monthlyUsage1, int monthlyUsage2, int monthlyUsage3)
+		{
+			this.customerId = customerId;
+			this.monthlyUsage1 = monthlyUsage1;
+			this.monthlyUsage2 = monthlyUsage2;
+			this.monthlyUsage3 = monthlyUsage3;
+		}
+
+		//Method to calculate the total monthly water usage
+		public int CalculateTotal()
+		{
+			return monthlyUsage1 + monthlyUsage3 + monthlyUsage3;
+		}
+
+		//Method to calculate the average monthly water usage
+		public double CalculateAverage()
+		{
+			return CalculateTotal() / 3.0;
+		}
+	}
+}
+
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
---
+++ PROG6221_ICETASK-3_ST10404816_Lisha_Naidoo/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_ICETASK_3_ST10404816_Lisha_Naidoo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("ICE Task 3\n");
+			Console.WriteLine("WATER MEASURING APPLICATION");
+			Console.WriteLine("*****************************");
+
+			string customerId;
+			int monthlyUsage1, monthlyUsage2, monthlyUsage3;
+
+			Console.Write("Enter the Customer ID >> ");
+			customerId = Console.ReadLine();
+			
+			Console.Write("Enter the water measure for Month 1 of Customer " +customerId+ " >> ");
+			monthlyUsage1 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter the water measure for Month 2 of Customer " + customerId + " >> ");
+			monthlyUsage2 = Convert.ToInt32(Console.ReadLine());
+			Console.Write("Enter the water measure for Month 3 of Customer " + customerId + " >> ");
+			monthlyUsage3 = Convert.ToInt32(Console.ReadLine());
+
+			ICETask2 ICETask2 = new ICETask2(customerId, monthlyUsage1, monthlyUsage2, monthlyUsage3);
+
+			Console.WriteLine("Total water usage: " +ICETask2.CalculateTotal());
+			Console.WriteLine("Average water usage: " +ICETask2.CalculateAverage());
+
+			Console.ReadLine();
+
+		}
+	}
+}
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
