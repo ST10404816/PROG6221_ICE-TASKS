dcba177e08a7aad694a7455c86f020f29120e4c4
Lisha Naidoo <128091406+ST10404816@users.noreply.github.com>
2024-04-28 14:44:36 +0200

    ICE TASK 1

---
+++ PROG6221_ICETASK-1_ST10404816_Lisha_Naidoo/ICETask1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_ICETASK_1_ST10404816_Lisha_Naidoo
+{
+	internal class ICETask1
+	{
+		//Prints first and last name on seperate lines with Capital letters
+		public static void ToUpper(string n)
+		{
+			string[] names = n.Split(' ');
+			Console.WriteLine("---Printing fullname on seperate lines in captial letters---");
+			Console.WriteLine(names[0].ToUpper());
+			Console.WriteLine(names[1].ToUpper());
+			Console.WriteLine();
+
+		}
+
+		//Prints the date in 3 different formats
+		public static void Date()
+		{
+			Console.WriteLine("---Printing date in 3 different formats---");
+			Console.WriteLine(DateTime.Now.ToString("MM/dd/yyyy"));
+			Console.WriteLine(DateTime.Now.ToString("dddd, dd MMMM yyyy"));
+			Console.WriteLine(DateTime.Now.ToString("ddd, dd MM yyyy"));
+
+		}
+		
+	}
+}
+
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
---
+++ PROG6221_ICETASK-1_ST10404816_Lisha_Naidoo/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROG6221_ICETASK_1_ST10404816_Lisha_Naidoo
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			//ICETask1 ICETask1 = new ICETask1();
+
+			//Asks the user to enter their full name
+			Console.WriteLine("ICE Task 1\n");
+
+			Console.Write("Please enter your fullname: ");
+			string fullName = Console.ReadLine();
+			ICETask1.ToUpper(fullName);
+			ICETask1.Date();
+			Console.ReadLine();
+
+		}
+	}
+}
+
+/*** References ***/
+
+/*
+ * Troelsen, A. and Japikse, P. 2021. Pro C# 9 with .NET 5: Foundational Principles and Practices in Programming. 10th ed. New York: Apress
+ */
